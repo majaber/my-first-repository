cf146da2d7f55f671b5425735f7494bb41bc9e3e
Moe Jaber <mjaber18@gmail.com>
2020-10-12 09:12:51 -0700

    Another practice attempt at pushing my directory over to the remote repository on gitHub.

---
+++ practice.js
@@ -0,0 +1,15 @@
+/*
+** This is a multi-line comment.  I'm at a loss for words, so I'm just
+** going to write some nonsense. Dig it?
+*/
+if (isNegativeNumber(value)) {
+  return false; /* We don't know how to handle negative numbers */
+}
+
+// Assigning a variable
+let fourAndTwentyBlackBirds = 24;
+
+// Defining a function
+function thisIsAGreatMethod() {
+  // Do some stuff
+}
