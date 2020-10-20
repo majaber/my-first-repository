1c3fde194ab61c3b109f428d7720bb1c241a0ddd
Moe Jaber <mjaber18@gmail.com>
2020-10-20 07:56:35 -0700

    daily notes 10/20/20

---
+++ codecademy.js
@@ -0,0 +1,12 @@
+/* Using this as a file to keep daily notes while working through Codecademy
+material. Ideally i'd like to continue practicing with github and the commands
+for push/pull requests */
+
+/* Something i'd like to review is why you can 'call' a function after you 'run'
+the function. For example:
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+sayThanks('Cole');
+*/
